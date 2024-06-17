4084185b00e70f81a9cbc549691738b258d4c6b4
Albert Albala <albert@mediatribe.net>
2024-06-17 16:21:48 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -12,6 +12,6 @@ curl -I http://0.0.0.0:8705/unsafe/500x/webserver/large-image.jpg | grep 200
 
 echo "Confirming that we cannot get an image from other sources"
 
-curl -I http://0.0.0.0:8705/unsafe/x100/i.imgur.com/iRcAbA9.png | grep 200 && exit 1
+curl -I http://0.0.0.0:8705/unsafe/500x/webserver_no_access/large-image.jpg | grep 200 && exit 1
 
 echo "All done with tests!"
