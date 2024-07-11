096e3573f081f291f209be1849b22d9f20ea8154
Suresh kumara <kumara.suresh25@gmail.com>
2024-07-11 22:30:35 +0530

    Making sure data-size="..." is used to fetch the optimized image, or use the original #17 (#18)
    Co-authored-by: SKN <skn@SKNs-MacBook-Pro.local>
    Co-authored-by: Albert Albala <albert@mediatribe.net>

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -34,7 +34,7 @@ export THUMBOR_SECURITY_KEY="$THUMBOR_SECURITY_KEY"
 
 ./scripts/generate-image-map.sh ./app/website-with-large-image webserver 200x ./app/unversioned/this-is-a-test2.json
 
-./scripts/generate-image-map.sh ./app/website-with-large-image webserver 500x ./app/website-with-large-image/unversioned-image-mapping.json
+./scripts/generate-image-map.sh ./app/website-with-large-image webserver 200x,x500,200x500,300x200 ./app/website-with-large-image/unversioned-image-mapping.json
 
 echo "Confirming that the unoptimized-to-optimized script works as expected"
 
