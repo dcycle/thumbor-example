2bd17e2dc5927a71684f76ebda90ce92543f164f
Suresh kumara <kumara.suresh25@gmail.com>
2025-06-08 23:29:18 +0530

    Delete scripts/merge-image-map-json-files.sh

--- scripts/merge-image-map-json-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Run ./merge-image-map-json-files.py in a Docker container
-#
-
-docker run -v $(pwd):/app --rm --entrypoint /bin/sh python:3-alpine -c \
-  "python3 /app/scripts/merge-image-map-json-files.py $1 $2 $3"
